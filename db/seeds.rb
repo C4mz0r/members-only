587de10b84b4312fd31562477d68b7ff02b17403
C4mz0r <aut0x@hotmail.com>
2015-11-01 20:19:24 -0700

    some database seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+a = User.create(name: "Alfred", email: "alf@example.com", password: "foobar", password_confirmation: "foobar" )
+b = User.create(name: "Bobo", email: "bobo@example.com", password: "foobar", password_confirmation: "foobar" )
+c = User.create(name: "Carol", email: "carol@example.com", password: "foobar", password_confirmation: "foobar" )
+
+a.posts.create(title: "I am the best", body: "I am #1.  I rule!")
+b.posts.create(title: "So boring", body: "This message board is crappy.")
+b.posts.create(title: "Alfred is annoying", body: "He thinks he's so great.  He is so full of himself.  Just shut up, dude.")
+c.posts.create(title: "Hey everyone", body: "I am glad to be part of this group")
